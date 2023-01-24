68ec01ce997f8fcff17cceb88a20ec3c0935bf18
JangDongGuk <jangdong5679@gmail.com>
2023-01-24 19:45:15 +0900

    setting 설정추가 및 logger middleware 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LoggerMiddeware } from './middlewares/logger.middleware';
 
 @Module({
   imports: [
@@ -12,4 +13,8 @@ import { AppService } from './app.service';
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule{
+  configure(consumer: MiddlewareConsumer): any {
+    consumer.apply(LoggerMiddeware).forRoutes('*');
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,8 @@ declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port = process.env.PORT || 3000;
-  await app.listen(3000);
+  const port = process.env.PORT || 3030;
+  await app.listen(3030);
   console.log(`서버가동중 포트번호 ${port}`);
 
   if (module.hot) {
